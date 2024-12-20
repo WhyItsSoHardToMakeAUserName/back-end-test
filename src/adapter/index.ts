52d8b2694f5b6701a588127ddcea6e8b43f15194
WhyItsSoHardToMakeAUserName <fox125j83773@gmail.com>
2024-12-21 04:37:17 +0500

    all new repositories  added  into buildAdapter

--- src/adapter/index.ts
+++ src/adapter/index.ts
@@ -1,18 +1,31 @@
 import { buildExampleGateway, ExampleGateway } from './gateway/example';
+import { buildFeedbackPostRepository, FeedbackPostRepository } from './repository/feedbackPost';
+import { buildFeedbackPostCategoryRepository, FeedbackPostCategoryRepository } from './repository/feedbackPost/category';
+import { buildFeedbackPostUpvoteRepository, FeedbackPostUpvoteRepository } from './repository/feedbackPost/upvote';
 import { buildUserRepository, UserRepository } from './repository/user';
 import { AdapterParams } from './types';
 
 export type Adapter = {
   userRepository: UserRepository;
   exampleGateway: ExampleGateway;
+  feedbackPostRepository: FeedbackPostRepository,
+  feedbackPostCategoryRepository: FeedbackPostCategoryRepository
+  feedbackPostUpvoteRepository:  FeedbackPostUpvoteRepository
 }
 
 export const buildAdapter = (params: AdapterParams): Adapter => {
   const userRepository = buildUserRepository(params);
+  const feedbackPostRepository = buildFeedbackPostRepository(params)
+  const feedbackPostCategoryRepository = buildFeedbackPostCategoryRepository(params)
+  const feedbackPostUpvoteRepository = buildFeedbackPostUpvoteRepository(params)
+  
   const exampleGateway = buildExampleGateway(params);
 
   return {
     userRepository,
-    exampleGateway
+    feedbackPostRepository,
+    feedbackPostCategoryRepository,
+    exampleGateway,
+    feedbackPostUpvoteRepository
   }
 }
